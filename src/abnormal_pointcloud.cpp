859196d3b272ad7538c97674f174a92af70145fa
shimjaehyung <shim5776@naver.com>
2022-12-21 18:19:38 +0900

    abnormal_pointcloud

--- src/abnormal_pointcloud.cpp
+++ src/abnormal_pointcloud.cpp
@@ -21,15 +21,17 @@
 // PCL Headers
 #include <pcl/common/common_headers.h>
 #include <pcl/features/normal_3d.h>
-#include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/common/common_headers.h>
+#include <pcl/common/transforms.h>
 
+// https://github.com/bshoshany/thread-pool
 // generate thread pool with a number of cpus
 BS::thread_pool pool(std::thread::hardware_concurrency());
 
@@ -91,19 +93,18 @@ public:
   {
     auto yolo_returns = yolo->ret;
     abnormal_contours.clear();
-    if(abnormal_contours.capacity() > 10) abnormal_contours.shrink_to_fit(); // shrink memory
+    if(abnormal_contours.capacity() > 100) abnormal_contours.shrink_to_fit(); // shrink memory
     
-    std::cout<<"yolo returns size : "<<yolo_returns.size()<<std::endl;
+    // std::cout<<"yolo returns size : "<<yolo_returns.size()<<std::endl;
     for(auto yolo_ret : yolo_returns)
     {
       // cls
       // 0 : abnormal
       // 1 : plant
       int16_t cls = yolo_ret.cls;
-
-      std::cout<<"cls : "<<cls<<" size : "<<yolo_ret.x.size()<<std::endl;
+      // std::cout<<"cls : "<<cls<<" size : "<<yolo_ret.x.size()<<std::endl;
       
-      if(cls != 0) continue;
+      if(cls != 0) continue; // only abnormal
       if(yolo_ret.x.size() <= 2) continue; //ignore empty contour
       if(yolo_ret.x.size() != yolo_ret.y.size()) throw std::invalid_argument("the nuber of x and y point different");
 
@@ -123,11 +124,13 @@ public:
   void intrinsics_callback(const sensor_msgs::CameraInfoPtr &intrinsic)
   {
     K = intrinsic->K;
+    D = intrinsic->D;
   }
   // subscribe empty topic. this is for 
   void empty_callback(const std_msgs::EmptyPtr &empty)
   {
     pool.push_task(&SeperateAbnormal::image_pipeline, this, depth_image, abnormal_contours);
+    // image_pipeline(depth_image, abnormal_contours);
   }
   // make mask image with contour
   cv::Mat make_contour_mask(cv::Mat &depth_image, std::vector<std::vector<cv::Point>> contour, int idx)
@@ -135,7 +138,7 @@ public:
     cv::Mat contour_mask = cv::Mat::zeros(depth_image.size(), CV_16UC1);
     drawContours(contour_mask, contour, idx, cv::Scalar(int(std::pow(2,16)-1)), -1);
     cv::Mat abnormal_depth = cv::Mat::zeros(contour_mask.size(), CV_16UC1);
-    cv::bitwise_and(depth_image*16, contour_mask, abnormal_depth);
+    cv::bitwise_and(depth_image, contour_mask, abnormal_depth);
 
     // cv::imshow("depth_image", depth_image);
     // cv::imshow("contour_mask", contour_mask);
@@ -144,17 +147,18 @@ public:
     return abnormal_depth;
   }
   // convert depth image to pointcloud and publish
-  pcl::PointCloud<pcl::PointXYZ>::Ptr depth_to_pointcloud(cv::Mat depth_image)
+  pcl::PointCloud<pcl::PointXYZ> depth_to_pointcloud(cv::Mat depth_image)
   {
     sensor_msgs::PointCloud2 cloud_msg;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
     int width = depth_image.cols;
     int height = depth_image.rows;
-    cloud->width = width;
-    cloud->height = height;
-    cloud->is_dense = false;
-    cloud->points.resize(cloud->width * cloud->height);
+    cloud.clear();
+    // cloud.width = width;
+    // cloud.height = height;
+    cloud.is_dense = false;
+    // cloud.points.resize(cloud.width * cloud.height);
 
     // Get the camera intrinsics
     double fx = K.at(0);
@@ -166,29 +170,43 @@ public:
     {
       for (int u = 0; u < width; u++)
       {
-        // Get the depth of the current pixel
-        float depth = depth_image.at<float>(v, u);
-        // Skip invalid pixels (depth = 0)
-        if (depth <= 1.0e-5) continue;
+        // https://github.com/IntelRealSense/librealsense/blob/5e73f7bb906a3cbec8ae43e888f182cc56c18692/include/librealsense2/rsutil.h#L46
+        // get a data of an element from depth image
+        uint16_t depth = depth_image.at<uint16_t>(v, u);
+        // Skip over pixels with a depth value of zero, which is used to indicate no data
+        if(depth==0) continue;
 
-        // Convert the pixel to a point in the point cloud
+        float x = (u - cx) / fx;
+        float y = (v - cy) / fy;
+
+        // Apply distortion
+        // float r2 = x * x + y * y;
+        // float f = 1 + D.at(0) * r2 + D.at(1) * r2 * r2 + D.at(4) * r2 * r2 * r2;
+        // float ux = x * f + 2 * D.at(2) * x * y + D.at(3) * (r2 + 2 * x * x);
+        // float uy = y * f + 2 * D.at(3) * x * y + D.at(2) * (r2 + 2 * y * y);
+        // x = ux;
+        // y = uy;
+
         pcl::PointXYZ point;
-        point.x = (u - cx) * depth / fx;
-        point.y = (v - cy) * depth / fy;
-        point.z = depth;
+        point.x = float(depth * x / 1000.0);
+        point.y = float(depth * y / 1000.0);
+        point.z = float(depth / 1000.0);
 
-        // Add the point to the point cloud
-        cloud->points[v * width + u] = point;
+        // cloud.points[v * width + u] = point;
+        cloud.push_back(point); 
+        // std::cout<<"u : "<<u<<" | v : "<<v<<" | x : "<<point.x<<" | y : "<<point.y<<" | z : "<<point.z<<""<<std::endl;
       }
     }
+
+    // print_pc(cloud);
     return cloud;
   }
   // publish pointcloud
-  void publish_pointcloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+  void publish_pointcloud(pcl::PointCloud<pcl::PointXYZ> cloud)
   {
     sensor_msgs::PointCloud2 cloud_msg;
-    pcl::toROSMsg(*cloud, cloud_msg);
-    cloud_msg.header.frame_id = "abnormal";
+    pcl::toROSMsg(cloud, cloud_msg);
+    cloud_msg.header.frame_id = "camera_link";
     cloud_msg.header.stamp = ros::Time::now();
 
     pointcloud_pub.publish(cloud_msg);
@@ -201,11 +219,20 @@ public:
     for(int i=0; i<contours.size(); i++)
     {
       cv::Mat abnormal_depth = make_contour_mask(depth_image, contours, i);
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = depth_to_pointcloud(abnormal_depth);
+      pcl::PointCloud<pcl::PointXYZ> cloud = depth_to_pointcloud(abnormal_depth);
       publish_pointcloud(cloud);
     }
   }
-
+  template <class T>
+  void print_pc(pcl::PointCloud<T>& cloud)
+  {
+    int count = 0;
+    if(count>0) return;
+    for (const auto& pt: cloud.points){
+        cout << count++ << ": ";
+        cout <<pt.x << ", "<<pt.y << ", "<< pt.z << endl;
+    }
+  }
 private:
   ros::NodeHandle nh_; // local node handler
 
@@ -217,6 +244,7 @@ private:
 
   int image_w, image_h;
   boost::array<double, 9> K;        // camera intrinsics
+  std::vector<double> D;                 // distortion coefficients
   cv::Mat depth_image;
   std::vector<std::vector<cv::Point>> abnormal_contours;
 };
@@ -235,10 +263,12 @@ int main(int argc, char **argv)
   ros::NodeHandle nh; //main node handler
   ros::Publisher empty_pub = nh.advertise<std_msgs::Empty>("/empty", 1); //publish empty topic
   SeperateAbnormal sep_abn(depth_topic, intrinsic_topic, yolo_topic, pc_topic);
-  ros::Rate rate(10); // rate of empty publisher
+  ros::Rate rate(30); // rate of empty publisher
 
-  while(ros::ok()){
+  while(ros::ok())
+  {
     empty_pub.publish(std_msgs::Empty());
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
     ros::spinOnce();
     rate.sleep();
   }
